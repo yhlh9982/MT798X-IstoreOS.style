8b523cbb8be7ce0a1faf731a94196be36979ee13
Otto kai <121492489+yhlh9982@users.noreply.github.com>
2026-01-12 12:09:45 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -23,16 +23,17 @@ echo 'src-git nas_luci https://github.com/linkease/nas-packages-luci.git;main' >
 # 自定义默认网关，后方的192.168.61.1即是可自定义的部分
 sed -i 's/192.168.6.1/192.168.30.1/g' package/base-files/files/bin/config_generate
 
+# 自定义主机名
 #sed -i "s/hostname='ImmortalWrt'/hostname='360T7'/g" package/base-files/files/bin/config_generate
 
 # 固件版本名称自定义
 #sed -i "s/DISTRIB_DESCRIPTION=.*/DISTRIB_DESCRIPTION='OpenWrt By gino $(date +"%Y%m%d")'/g" package/base-files/files/etc/openwrt_release
 
 # 取消原主题luci-theme-bootstrap 为默认主题
-#sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
+sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
+
+# 删除原默认主题
+rm -rf package/lean/luci-theme-bootstrap
 
 # 修改 argon 为默认主题
 #sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
-
-# 删除原默认主题
-#rm -rf package/lean/luci-theme-bootstrap
