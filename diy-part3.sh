b499eb8f6564bef8cccdb48a9f4c5961ffc4e674
Otto kai <121492489+yhlh9982@users.noreply.github.com>
2026-02-20 11:28:43 +0800

    Update diy-part3.sh

--- diy-part3.sh
+++ diy-part3.sh
@@ -70,7 +70,7 @@ if [ -n "$KSMBD_FILES" ]; then
 fi
 
 # 修改默认 IP (192.168.30.1)
-sed -i 's/192.168.1.1/192.168.30.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.6.1/192.168.30.1/g' package/base-files/files/bin/config_generate
 
 # ----------------------------------------------------------------
 # 5. 【最关键一步】强制重新注册所有 Feeds
