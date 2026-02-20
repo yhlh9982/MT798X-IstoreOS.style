0033c2eb4b88950b3a57c7bad62cde7b3d8e955d
Otto kai <121492489+yhlh9982@users.noreply.github.com>
2026-02-20 12:04:08 +0800

    Create diy-part1.sh

---
+++ 21.02/diy-part1.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part1.sh
+# Description: OpenWrt DIY script part 1 (Before Update feeds)
+#
+# Copyright (c) 2019-2024 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+
+# 功能插件
+git clone --depth=1 https://github.com/iv7777/luci-app-authshield package/authshield
+git clone --depth=1 https://github.com/asvow/luci-app-tailscale package/tailscale
+
+# 添加 openwrt 18.06-21.02 插件库
+# git clone --depth=1 -b Immortalwrt https://github.com/makebl/openwrt-package  package/openwrt-package
+# git clone --depth=1 -b Immortalwrt https://github.com/shidahuilang/openwrt-package  package/openwrt-package
+
+# theme
+git clone --depth=1 -b openwrt-22.03 https://github.com/sbwml/luci-theme-argon package/argon
